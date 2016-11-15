c9d0a5541367b625ecc3a238f6b686bbe3b4c8dd
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-15 12:42:57 -0500

    added comment, heart, destroy, update, and create actions to lecture_request controller

--- app/controllers/lecture_requests_controller.rb
+++ app/controllers/lecture_requests_controller.rb
@@ -12,10 +12,69 @@ class LectureRequestsController < ApplicationController
     render json: @lectureRequest
   end
 
+  def create
+    @lectureRequest = current_user.lecture_requests.build(title: lecture_request_params[:title], content: lecture_request_params[:content])
+    if @lectureRequest.save
+      flash.now[:notice] = 'Thank you! Your Lecture Request was posted.'
+      # rendering the object, unless redirecting to show page then we wont need this line below
+      render json: @lectureRequest, status: 201, root: true
+    else
+      render json: @lectureRequest, status: 404
+      # try - render json: {errors: @lectureRequest.errors.full_messages}, status: unprocessable_entity
+    end
+  end
+
+  def update
+    if @lectureRequest.save
+      flash.now[:notice] = 'Thank you! Your Lecture Request was updated.'
+    else
+      render json: @lectureRequest, status: 404
+      # try - render json: {errors: @lectureRequest.errors.full_messages}, status: unprocessable_entity
+    end
+  end
+
+  def heart_lecture_request
+    @lectureRequest.user_likes.build(user_id: current_user.id)
+    if @lectureRequest.save
+      flash.now[:notice] = 'Thank you! You liked this Lecture Request.'
+    else
+      render json: @lectureRequest, status: 404
+      # try - render json: {errors: @lectureRequest.errors.full_messages}, status: unprocessable_entity
+    end
+  end
+
+  def comment_lecture_request
+    @lectureRequest.comments.build(lecture_request_params[:comments])
+    if @lectureRequest.save
+      flash.now[:notice] = 'Thank you! Your comment was posted to this Lecture Request.'
+    else
+      render json: @lectureRequest, status: 404
+      # try - render json: {errors: @lectureRequest.errors.full_messages}, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @lectureRequest.user == current_user.id
+      Comment.where("lecture_request_id = ?", @lectureRequest.id).each { |comment| comment.destroy }
+      Heart.where("lecture_request_id = ?", @lectureRequest.id).each { |heart| heart.destroy }
+      if @lectureRequest.destroy
+        flash.now[:notice] = 'You\'ve deleted this Lecture Request.'
+      else
+        render json: @lectureRequest, status: 404
+        # try - render json: {errors: @lectureRequest.errors.full_messages}, status: unprocessable_entity
+      end
+    end
+  end
+
   private
 
-  def set_lecture_request
-    @lectureRequest = LectureRequest.find(params[:id])
+    def set_lecture_request
+      @lectureRequest = LectureRequest.find(params[:id])
+    end
+
+  def lecture_request_params
+    params.require(:lecture_request).permit(:title, :content)
+  # might need to add comments
   end
 
 end
