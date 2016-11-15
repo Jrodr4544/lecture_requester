2b71cb2610b00afaded8b3b37bf5ac7738527ca2
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-15 10:34:22 -0500

    added route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit]
 
   resources :lecture_requests, only: [:create, :show, :update, :index, :destroy]
 end
