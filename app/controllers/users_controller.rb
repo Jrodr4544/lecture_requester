425f13a0f131c84df0de89518ab42ada11abf9c3
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-15 10:53:25 -0500

    initial commit for users controller with show action defined

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
-  before_action authenticate_user!
+  # before_action authenticate_user, only: [:edit]
+  before_action :set_user
 
-  
+  def show
+    @user
+    render json: @user
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
