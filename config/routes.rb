09e6c38d234c74d375b6cba7e93b675b5adeb00e
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-15 12:46:53 -0500

    added post routes to comment and heart

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit]
 
   resources :lecture_requests, only: [:create, :show, :update, :index, :destroy]
+
+  post 'lecture_requests/:id/heart', to: 'lecture_request#heart_lecture_request'
+  post 'lecture_requests/:id/comment', to: 'lecture_request#comment_lecture_request'
+
 end
