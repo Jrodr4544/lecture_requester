d8e8e8de1f402e5b0fc9963408115c163ef488d1
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-15 11:18:27 -0500

    added show action to lecture_request controlller

--- app/controllers/lecture_requests_controller.rb
+++ app/controllers/lecture_requests_controller.rb
@@ -1,10 +1,21 @@
 class LectureRequestsController < ApplicationController
   before_action :authenticate_user, only: [:create, :destroy, :edit]
+  before_action :set_lecture_request, only: [:show]
 
   def index
     @lectureRequests = LectureRequest.all
 
     render json: @lectureRequests
+  end
+
+  def show
+    render json: @lectureRequest
+  end
+
+  private
+
+  def set_lecture_request
+    @lectureRequest = LectureRequest.find(params[:id])
   end
 
 end
