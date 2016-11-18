08df823a55e1451ede205d3114253dee43f3c9a3
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-17 20:01:21 -0500

    initial commit for angular routes

---
+++ app/assets/javascripts/routes.js
@@ -0,0 +1,30 @@
+// using the iffy (function) loads the code after the dom loads
+( function() {
+  'use strict';
+  
+  angular
+    .module('lecture_requester')  
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+              url: '/',
+              templateUrl: 'home/home.html',
+              controller: 'HomeController as vm'
+            })
+            // nested route below at home.lecture_requests
+            .state('home.user', {
+              url: '/user',
+              templateUrl: 'user/profile.html',
+              controller: 'UserProfileController as vm'
+            })
+            .state('home.about', {
+              url: 'about',
+              templateUrl: 'user/profile.html',
+              // will i need an about page controller?
+              controller: 'HomeController as vm'
+            })
+
+        $urlRouterProvider.otherwise('/')
+    })
+
+}());
