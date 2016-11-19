8b859cba7e43476e19345a8628eff66200653fb9
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-19 11:40:01 -0500

    added configuration for devise controller to repond to json in order for front end authentication(angularjs) to work

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module LectureRequester
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  
+    # Allowing Devise to respond to JSON
+
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
+
   end
 end
