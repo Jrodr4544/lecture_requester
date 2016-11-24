d9d85153c8c2ac04c3151ce522145828f1a923d8
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-24 10:18:33 -0500

    commented out the all, create and find functions because I might just use the controller for these

--- app/assets/javascripts/services/lectureRequests.service.js
+++ app/assets/javascripts/services/lectureRequests.service.js
@@ -2,42 +2,49 @@
 
   'use strict';
 
-  function LectureRequestsService($scope, user) {
+  function LectureRequestsService($scope, user, $resource) {
 
-    // vm.update     = function() {
+    
+
+    var LectureRequest = $resource('/lecture_request/:id', {id: '@id'}, {
+      get: {method: 'GET'}, 
+      update: {method: 'PUT'},
+      delete: {method: 'DELETE'}
+    });
+
+
+
+    return LectureRequest;
+
+    // function createRequest() {
     //   debugger
-    //   UserFactory.updateUser(vm.current_user);
+    //   var req = {
+    //     method: 'POST',
+    //     url: '/lecture_requests/'+user.id,
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     data: {
+    //       user: user
+    //     }
+    //   }
+    //   debugger
+
+    //   return $http(req)
+    //     .catch(handleError);
     // }
 
-    function createRequest() {
-      debugger
-      var req = {
-        method: 'POST',
-        url: '/lecture_requests/'+user.id,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        data: {
-          user: user
-        }
-      }
-      debugger
+    // function all() {
+    //     return $http.get('/lecture_requests/index.json',{}).success(function(data) {});
+    // }
 
-      return $http(req)
-        .catch(handleError);
-    }
+    // function all() {
+    //     return $http.get('/lecture_requests/index.json',{}).success(function(data) {});
+    // }
 
-    function all() {
-        return $http.get('/lecture_requests/index.json',{}).success(function(data) {});
-    }
-
-    function all() {
-        return $http.get('/lecture_requests/index.json',{}).success(function(data) {});
-    }
-
-    function find() {
-        return $http.get('/lecture_requests/index.json',{}).success(function(data) {});
-    }
+    // function find() {
+    //     return $http.get('/lecture_requests/index.json',{}).success(function(data) {});
+    // }
 
     // They can be called like this:
 
