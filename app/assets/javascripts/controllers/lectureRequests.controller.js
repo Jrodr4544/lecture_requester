d51693858235f27c3f7eb80802d17c75679b6349
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-11-27 14:15:55 -0500

    added functions that communicate with LectureRequestsFactory methods

--- app/assets/javascripts/controllers/lectureRequests.controller.js
+++ app/assets/javascripts/controllers/lectureRequests.controller.js
@@ -2,36 +2,65 @@
 ( function() {
   'use strict';
   
-  function LectureRequestsController($scope, $http){
+  function LectureRequestsController($scope, $http, LectureRequestsFactory){
     $scope.request = "I am making a request to learn more about rails";
     
     var vm = this;
 
-    // vm.lectureRequest = {
-    //   title: '',
-    //   content: ''
-    // }
     vm.allRequests;
+    vm.comment;
+    
+    // $scope.lectureRequest.title   = '';
+    // $scope.lectureRequest.content = '';
 
-    getRequests()
+    vm.getRequests   = getRequests;
+    vm.getRequest    = getRequest;
+    vm.createRequest = createRequest;
+    vm.updateRequest = updateRequest;
 
     function getRequests() {
-      // no ssl
-      return $http.get('http://localhost:3000/lecture_requests.json')
-                  .then(function(data) {
-                    debugger
-                    console.log(data);
-                    vm.allRequests = data.data;
-                  })
+      debugger
+      return LectureRequestsFactory.getRequests()
+              .then(setRequests)
     }
 
-    vm.create = function() {
-      debugger
-      vm.lectureRequest.title = '';
+    function getRequest() {
+      return LectureRequestsFactory.getRequest()
+              .then(/*callback function*/)
     }
+
+    function createRequest() {
+      debugger
+      return LectureRequestsFactory.createRequest($scope.lectureRequest)
+              .then(function(response){
+                console.log(response);
+              })
+    }
+
+    function updateRequest() {
+      return LectureRequestsFactory.updateRequest()
+              .then(/*callback function*/)
+    }
+
+    function setRequests(data) {
+      return vm.allRequests = data; // unless response is already data.data then this should be set as data
+    }
+
+
+    $scope.submitComment = function($event) {
+      debugger
+        alert('submitting comment');
+        vm.comment = $event.value;
+        console.log($event);
+        console.log($scope.username);
+    }
+    // vm.create = function() {
+    //   debugger
+    //   vm.lectureRequest.title = '';
+    // }
   }
 
-  LectureRequestsController.$inject = ['$scope', '$http']
+  LectureRequestsController.$inject = ['$scope', '$http', 'LectureRequestsFactory']
 
   angular
     .module('lecture_requester')  
