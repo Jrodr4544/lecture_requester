27d9df299438dfdb632bb85bb9cc68597c2f4a8e
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-12-09 13:20:10 -0500

    added a commenter_username to serializer in order to show the comment's username

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,10 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :text
+  attributes :id, :text, :user_id, :lecture_request_id, :commenter_username
 
   belongs_to :lecture_request_id
   belongs_to :user_id
+
+  def commenter_username
+    object.user.username
+  end
 end
