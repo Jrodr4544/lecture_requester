37df486e0d806288772a0311e49a76636adb6a7a
Jrodr4544 <jelson.rodriguez@gmail.com>
2017-02-17 13:12:30 -0500

    commented out filter not in use

--- app/assets/javascripts/filters/lectureRequest.filter.js
+++ app/assets/javascripts/filters/lectureRequest.filter.js
@@ -1,14 +1,18 @@
-function lectureRequestFilter() {
-  return function (items, lectureRequest) {
-    debugger
-    return items.filter(function (item) {
-      debugger
-      // this filter is not in use yet
-      return item.location. === lectureRequest;
-    })
-  };  
-}
+// function lectureRequestFilter() {
+//   return function (items, lectureRequest) {
+//     debugger
+//     return items.filter(function (item) {
+//       debugger
+//       // this filter is not in use yet
+//       return item.location. === lectureRequest;
+//     })
+//   };  
+// }
 
-angular
-  .module('app')
-  .filter('lectureRequestFilter', lectureRequestFilter);
+// angular
+//   .module('app')
+//   .filter('lectureRequestFilter', lectureRequestFilter);
+
+
+
+// NOT IN USE
