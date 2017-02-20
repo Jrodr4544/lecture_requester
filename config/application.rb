f28a6c4f2d941d936ed433b9e348bacc8c4fbbe3
Jrodr4544 <jelson.rodriguez@gmail.com>
2017-02-20 16:52:05 -0500

    removed config for default headers

--- config/application.rb
+++ config/application.rb
@@ -20,12 +20,6 @@ module LectureRequester
     config.to_prepare do
       DeviseController.respond_to :html, :json
     end
-
-    # Setting up headers to avoid CORS issues
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-    }
     
   end
 end
