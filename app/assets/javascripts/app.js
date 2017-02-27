96f839dfd94b2a5db767cdc1227d48cda5df7efd
Antonio David <antonio.g.david@gmail.com>
2017-02-27 17:34:25 -0500

    Update app.js
    Addition to front-end for X-CSRF-Token

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,4 +4,8 @@
   
   angular
     .module('lecture_requester', ['ui.router','templates','Devise', 'asset-path'])  
+    .config(['$httpProvider', function($httpProvider) {
+          // for CSRF errors
+          $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+      }])
 }());
