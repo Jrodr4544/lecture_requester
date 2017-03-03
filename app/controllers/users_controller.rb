d32a85aa97700e4263af0f87b7581811aed0349a
Jrodr4544 <jelson.rodriguez@gmail.com>
2017-03-03 11:25:15 -0500

    removed unnecessary binding.prys that were causing an internal error 500 code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,8 @@ class UsersController < ApplicationController
   # before_action authenticate_user, only: [:edit]
 
   def show
-    binding.pry
     if user_signed_in?
       respond_to do |format|
-        format.html { render :show }
         format.json { render json: current_user, include: ['lecture_requests.comments', 'lecture_requests.user_likes']}
       end
     end
@@ -16,7 +14,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    binding.pry
     @user = @trip.objectives.find(params[:id])
 
     if @user.update(params)
