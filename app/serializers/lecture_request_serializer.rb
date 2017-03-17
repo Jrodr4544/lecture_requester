b974e6b6d124e1a7f3584edf2a1d684794dca6d5
Jrodr4544 <jelson.rodriguez@gmail.com>
2017-03-17 16:04:59 -0400

    added belongs_to user in order to show the user_id

--- app/serializers/lecture_request_serializer.rb
+++ app/serializers/lecture_request_serializer.rb
@@ -1,7 +1,8 @@
 class LectureRequestSerializer < ActiveModel::Serializer
   attributes :id, :content, :title
 
-  has_many  :comments
-  has_many  :user_likes
+  has_many    :comments
+  has_many    :user_likes
+  belongs_to  :user
 end
 #  might need a ser serializer
