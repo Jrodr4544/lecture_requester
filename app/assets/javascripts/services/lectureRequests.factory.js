06503188237e9875d21d28a90991e970de0eaf30
Jrodr4544 <jelson.rodriguez@gmail.com>
2017-03-17 16:02:29 -0400

    service is now used for promises. Changed 'vm' to 'self' and updated all references to it - service is not a view-model. Removed the getUserRequests because a filter on allRequests for the appropriate user is more efficient. getRequests function now uses a promise to resolve data after the ajax request completes, and takes an argument so it knows when an update is required so that it doesn’t resolve from cache. createRequest function now takes a user_id as an argument so that once the request is created and after we repopulate the requests, we can update the userRequests. setRequests now also pushes the user_id into allRequests in order to filter based on user. setUserRequests filters through allRequests based on user_id and broadcasts the updated userRequests. removeRequest takes the user in the ajax response and passes it to setUserRequests in order to filter user’s requests after allRequests gets repopulated

--- app/assets/javascripts/services/lectureRequests.factory.js
+++ app/assets/javascripts/services/lectureRequests.factory.js
@@ -2,19 +2,18 @@
 
   'use strict';
 
-  function LectureRequestsFactory($rootScope,$http) {
-    var vm = this;
+  function LectureRequestsFactory($rootScope,$http,$q) {
+    var self = this;
 
     // object below can be referenced by other controllers to allow access to methods and array containers
     var LectureRequestsFactory = {};
-        LectureRequestsFactory.allRequests    = [];
-        LectureRequestsFactory.userRequests   = [];
+        self.allRequests    = [];
+        self.userRequests   = [];
 
     // return LectureRequestsFactory; // not needed because the return below is doing it :-)
     
     return {
       LectureRequestsFactory: LectureRequestsFactory,
-             getUserRequests: getUserRequests,
                removeRequest: removeRequest,
                  getRequests: getRequests,
                likedRequests: likedRequests,
@@ -23,22 +22,35 @@
                 heartRequest: heartRequest
     }
 
-    function getRequests() {
-      // no ssl
-      $http.get('/lecture_requests.json')
-                  .then(setRequests);
-    }
+    function getRequests(updated = false) {
+      var deferred = $q.defer();
+      
+      // If we already have the requests, we can resolve the promise.
+      // but when we delete we pass the true value to updated argument.
+      if (self.allRequests.length !== 0 && updated == false) {
+        deferred.resolve(self.allRequests); // Resolving from cache
+      } else {
+        // no ssl
+        $http.get('/lecture_requests.json')
+          .success(function(data){
+            self.allRequests = []; // Resetting allRequests when get request 
+                                   // completes to avoid duplicate entries.
+            setRequests(data);     // Setting allRequests
+            deferred.resolve(self.allRequests); // From server. At this point we have the requests.
+          })
+          .error(function(response){
+            deferred.reject(response);
+          });
+      }
 
-    function getUserRequests(user) {
-      return $http.get('/users/'+user.id+'.json')
-                  .then(setUserRequests);
+      return deferred.promise
     }
 
     function heartRequest(data) {
       // This will store the data in hearts for this service which can then be accessed. 
       $http.post('/lecture_requests/'+data.lecture_request_id+'/heart', data).then(function(data){
       // This variable gets the request in allRequests that matches the response and then the user_likes for it gets updated
-        var request = LectureRequestsFactory.allRequests.filter(function(request){
+        var request = self.allRequests.filter(function(request){
                       // finding the request id in .allRequests that matches the ajax response(data) id
                         if (request.id == data.data.id) {return request};
                       })[0];
@@ -61,7 +73,7 @@
 
       return $http(req)
                 .then(function(data){
-                  var request = LectureRequestsFactory.allRequests.filter(function(request){
+                  var request = self.allRequests.filter(function(request){
                                   if (request.id == data.data.id) {return request};
                                 })[0];
                   request.comments = data.data.comments;
@@ -73,10 +85,10 @@
     function likedRequests(user) {
       var requests = [];
       
-      LectureRequestsFactory.allRequests.filter(function(request){
+      self.allRequests.filter(function(request){
         // going through each request's user_likes to grab the user's liked requests
         var likes = request.user_likes;
-
+        debugger
         for (var i = 0; i < likes.length; i++) {
           if (likes[i].id == user.id) {requests.push(request)};
         }
@@ -85,7 +97,7 @@
       return requests;
     }
 
-    function createRequest(request) {
+    function createRequest(request, user_id = null) {
       var req = {
         method: 'POST',
         url: '/lecture_requests',
@@ -98,50 +110,54 @@
       }
 
       return $http(req)
+                .success(function(response){
+                  getRequests(true).then(function(){
+                    user_id == null ? null : setUserRequests(user_id);
+                  });
+                })
                 .catch(handleError)
     }
 
     // This is a callback function that stores a response and sets it as the service's allRequests
     function setRequests(data) {
-      var requests = data.data;
-      if (requests.length != LectureRequestsFactory.allRequests.length) {
+      var requests = data;
+
+      if (requests.length != self.allRequests.length) {
         for (var i = 0; i < requests.length; i++) {
           requests[i]
           // pushing the response data into allRequests
-          LectureRequestsFactory.allRequests.push({id: requests[i].id, content: requests[i].content, title: requests[i].title, comments: requests[i].comments, user_likes: requests[i].user_likes})
-        }
-        // broadcasting the new requests when any changes get made
-        $rootScope.$broadcast('requests:updated', LectureRequestsFactory.allRequests);
-      }
-
-    }
-
-    // This is a callback function that stores a response and sets it as the service's userRequests
-    function setUserRequests(data) {
-      var requests = data.data.lecture_requests;
-      // This is resetting the userRequests
-      LectureRequestsFactory.userRequests = [];
-      if (requests.length != LectureRequestsFactory.userRequests.length) {
-      for (var i = 0; i < requests.length; i++) {
-          requests[i]
-          // pushing the response data into userRequests
-          LectureRequestsFactory.userRequests.push({id: requests[i].id, content: requests[i].content, title: requests[i].title, comments: requests[i].comments, user_likes: requests[i].user_likes})
+          self.allRequests.push({id: requests[i].id, content: requests[i].content, title: requests[i].title, comments: requests[i].comments, user_likes: requests[i].user_likes, user_id: requests[i].user.id})
         }
       }
-      // broadcasting the new requests when any changes get made
-      $rootScope.$broadcast('user_requests:updated', LectureRequestsFactory.userRequests);
     }
 
-    // This function can be called from a view to return the service's current requests
-    function requests() {
-      return vm.allRequests;
+    // This function sets the service's userRequests
+    function setUserRequests(user_id) {
+
+      self.userRequests = self.allRequests.filter(function(request){
+                            // finding the request id in .allRequests that matches the id passed as argument
+                            if (request.user_id == user_id) {return request};
+                          });
+     
+      // broadcasting the updated userRequests
+      $rootScope.$broadcast('user_requests:updated', self.userRequests);
+      return self.userRequests;
     }
 
     function removeRequest(data) {
+      var user;
+
       $http.delete('/lecture_requests/'+data).then(function(response){
-        console.log(response.data);
-        getUserRequests(response.data);
+        user = response.data;  
+        // getting requests to refresh the allRequests dataset
+        // the true value is passed so that it knows it was updated
+        // getting the requests after the delete completes.
+        getRequests(true).then(function(){
+          // updating userRequests after allRequests update.
+          setUserRequests(user.id);
+        });      
       });
+      
     }
 
     function handleError(response) {
